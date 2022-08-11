85f2a1456ac70debf4ad6f5c44f46360560591b9
Alex <alexjuniorjn@hotmail.com>
2022-08-11 17:53:29 -0300

    Agregando o localstorage ao App e adicionando a função inserir tarefa com o Enter

--- src/script.js
+++ src/script.js
@@ -33,6 +33,7 @@ const addTask = () => {
 
     inputElement.value = "";
 
+    updateLStorage()
 }
 
 const completeTask = (taskContentText) => {
@@ -43,6 +44,8 @@ const completeTask = (taskContentText) => {
             task.classList.toggle('completed');
         }
     }
+
+    updateLStorage()
 }
 
 const deleteTask = (taskItemContainer, taskContentText) => {
@@ -53,7 +56,54 @@ const deleteTask = (taskItemContainer, taskContentText) => {
             taskItemContainer.remove();
         }
     }
+
+    updateLStorage()
 }
+
+const updateLStorage = () => {
+    const tasks = tasksContainer.childNodes;
+
+    const tasksLStorage = [...tasks].map((task) => {
+        const content = task.firstChild;
+        const isCompleted = task.classList.contains("completed");
+
+        return {description: content.innerText, isCompleted};
+    });
+
+    localStorage.setItem('tasks', JSON.stringify(tasksLStorage));
+}
+
+const refreshScreenWithLStorage = () => {
+    const tasksFromLStorage = JSON.parse(localStorage.getItem('tasks'));
+
+    if(!tasksFromLStorage) return;
+
+    for (const task of tasksFromLStorage) {
+        const taskItemContainer = document.createElement('div');
+        taskItemContainer.classList.add('task-item');
+
+        if(task.isCompleted) {
+            taskItemContainer.classList.add('completed');
+        }
+        
+        const taskContentText = document.createElement('p');
+        taskContentText.innerText = task.description;
+
+        taskItemContainer.addEventListener('click', () => completeTask(taskContentText));
+
+        const taskDeleteButton = document.createElement('img');
+        taskDeleteButton.setAttribute('src', '/assets/icons/circle-minus.svg');
+
+        taskDeleteButton.addEventListener('click', () => deleteTask(taskItemContainer, taskContentText));
+
+        taskItemContainer.appendChild(taskContentText);
+        taskItemContainer.appendChild(taskDeleteButton);
+        
+        tasksContainer.appendChild(taskItemContainer);
+    }
+}
+
+refreshScreenWithLStorage();
 
 const inputChange = ()=> {
     const inputIsValid = validadeInput();
@@ -62,6 +112,15 @@ const inputChange = ()=> {
         return inputElement.classList.remove("error")
     }
 }
+
+const addWithEnter = (event) => {
+    const key = event.key;
+    if (key === 'Enter') {
+        addTask();
+    }
+}
+
+inputElement.addEventListener('keypress', addWithEnter);
 
 addTaskButton.addEventListener('click', () => addTask())
 inputElement.addEventListener('change', () => inputChange())
